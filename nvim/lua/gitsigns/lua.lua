0c2c533e8d3a27a0e1036101e572291627cbb086
Kerem Vatandas <keremvatandas@gmail.com>
2021-04-16 10:15:08 +0300

    nextgen nvim conf with lua lang

---
+++ nvim/lua/gitsigns/lua.lua
@@ -0,0 +1,34 @@
+require("gitsigns").setup {
+    signs = {
+    -- TODO add hl to colorscheme
+    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    },
+    numhl = false,
+    keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
+        ["n ]c"] = {expr = true, '&diff ? \']c\' : \'<cmd>lua require"gitsigns".next_hunk()<CR>\''},
+        ["n [c"] = {expr = true, '&diff ? \'[c\' : \'<cmd>lua require"gitsigns".prev_hunk()<CR>\''},
+        ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+        ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+        ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line()<CR>'
+    },
+    watch_index = {
+        interval = 100
+    },
+    sign_priority = 5,
+    status_formatter = nil -- Use default
+}
+
+local cmd = vim.cmd
+
+cmd "hi DiffAdd guifg=#81A1C1 guibg = none"
+cmd "hi DiffChange guifg =#3A3E44 guibg = none"
+cmd "hi DiffModified guifg = #81A1C1 guibg = none"
