fd6855f8e0fb158d3fe86f25361c7c375ac48ff2
Kerem Vatandas <keremvatandas@gmail.com>
2021-04-17 01:44:58 +0300

    fixed lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,45 +8,54 @@ require("colorizer").setup()
 require("neoscroll").setup() -- smooth scroll
 
 -- lsp
-require "lspconfig.lua"
+require("lspinstall").setup()
 require "compe.lua"
+require "lspconfig.lua"
 
-local cmd = vim.cmd
-local g = vim.g
-local o = vim.o
-local w = vim.w
-local b = vim.b
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
 
 g.mapleader = " "
 g.auto_save = 1
 
-o.hidden = true
-o.ignorecase = true
-o.splitbelow = true
-o.splitright = true
-o.termguicolors = true
-o.numberwidth = 2
-o.mouse = "a"
-o.cmdheight = 1
-o.updatetime = 250
-o.clipboard = "unnamedplus"
-
-w.number = true
-w.signcolumn = "yes"
-
-b.shiftwidth = 2
-
+local indent = 4
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options (for deoplete)
+opt('o', 'hidden', true)                              -- Enable modified buffers in background
+opt('o', 'ignorecase', true)                          -- Ignore case
+opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4 )                             -- Lines of context
+opt('o', 'shiftround', true)                          -- Round indent
+opt('o', 'sidescrolloff', 8 )                         -- Columns of context
+opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)                          -- Put new windows below current
+opt('o', 'splitright', true)                          -- Put new windows right of current
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'ignorecase', true)                       -- True color support
+opt('o', 'mouse', 'a')                       -- True color support
+opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
+opt('w', 'number', true)                              -- Print line number
+opt('w', 'relativenumber', true)                      -- Relative line numbers
+opt('w', 'wrap', false)                               -- Disable line wrap
 
 -- colorscheme related stuff
 cmd "syntax enable"
 cmd "syntax on"
-
-local base16 = require "base16"
-base16(base16.themes["onedark"], true)
+cmd "colorscheme dracula"
 
 -- blankline
-local indent = 2
-
 g.indentLine_enabled = 1
 g.indent_blankline_char = "┊"
 
